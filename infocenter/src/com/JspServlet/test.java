bdc4fe263c3f447f06dbe69a11da4949cc69ff70
ganesan-sree <ganesan@easymedmobile.com>
2013-04-26 11:36:42 +0530

    Ibm infocenter

---
+++ infocenter/src/com/JspServlet/test.java
@@ -0,0 +1,15 @@
+package com.JspServlet;
+
+//import org.eclipse.equinox.http.servlet.internal.ProxyServlet;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	//	ProxyServlet pr= new ProxyServlet();
+
+	}
+
+}
